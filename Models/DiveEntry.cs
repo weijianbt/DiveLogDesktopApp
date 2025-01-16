74483615dd39d221093475e810bfcb68ee7cde2d
weijianbt <twj.bryan@gmail.com>
2025-01-16 23:25:10 +0800

    Add dive log entry initial design

---
+++ Models/DiveEntry.cs
@@ -0,0 +1,26 @@
+﻿using DiveLogApplication.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiveLogApplication.Models
+{
+    public class DiveEntry : ViewModel
+    {
+        public DiveEntry()
+        {
+            
+        }
+
+        public string DiveSite { get; set; }
+        public string Location { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public double Duration {  get; set; }
+        public double MaxDepth {  get; set; }
+        public double AverageDepth { get; set; }
+
+    }
+}
---
+++ ViewModels/DiveLogViewModel.cs
@@ -0,0 +1,136 @@
+﻿using DiveLogApplication.Core;
+using DiveLogApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiveLogApplication.ViewModels
+{
+    public class DiveLogViewModel : ViewModel
+    {
+        private string _diveSite;
+        private string _location;
+        private DateTime _startTime;
+        private DateTime _endTime;
+        private double _duration;
+        private double _maxDepth;
+        private double _averageDepth;
+        private List<DiveEntry> _diveLogList;
+
+        public DiveLogViewModel()
+        {
+            DiveLogList = new List<DiveEntry>
+            {
+                new DiveEntry()
+                {
+                    DiveSite = "Tioman",
+                    Location = "Tioman Mersing",
+                    StartTime = DateTime.Now,
+                    EndTime = DateTime.Now,
+                    Duration = 12,
+                    MaxDepth = 11.1,
+                    AverageDepth = 9
+                },
+
+                new DiveEntry()
+                {
+                    DiveSite = "Tenggol",
+                    Location = "Terrengganu",
+                    StartTime = DateTime.Now,
+                    EndTime = DateTime.Now,
+                    Duration = 45,
+                    MaxDepth = 18,
+                    AverageDepth = 10
+                }
+            };
+
+        }
+
+        public string DiveSite
+        {
+            get => _diveSite;
+            set
+            {
+                _diveSite = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Location
+        {
+            get => _location;
+            set
+            {
+                _location = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public DateTime StartTime
+        {
+            get => _startTime;
+            set
+            {
+                _startTime = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public DateTime EndTime
+        {
+            get => _endTime;
+            set
+            {
+                _endTime = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double Duration
+        {
+            get => _duration;
+            set
+            {
+                _duration = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double MaxDepth
+        {
+            get => _maxDepth;
+            set
+            {
+                _maxDepth = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double AverageDepth
+        {
+            get => _averageDepth;
+            set
+            {
+                _averageDepth = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public List<DiveEntry> DiveLogList
+        {
+            get => _diveLogList;
+            set
+            {
+                _diveLogList = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public override string ToString()
+        {
+            return DiveLogList[0].DiveSite.ToString();
+        }
+    }
+}
